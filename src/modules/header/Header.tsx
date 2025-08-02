0fc217220f9a4ee1f21ec1cd3fb5dc92467edc4d
Artem <skeleton.ubica@gmail.com>
2025-08-02 13:41:22 +0300

    first commit

---
+++ src/modules/header/Header.tsx
@@ -0,0 +1,7 @@
+import './Header.css'
+
+const Header = () => {
+  return <section className='header'>header</section>;
+};
+
+export default Header;
---
+++ src/pages/app/App.tsx
@@ -0,0 +1,13 @@
+import Header from "../../modules/header";
+import './App.css'
+
+const App = () => {
+  return (
+    <section className="app">
+      <Header />
+      App
+    </section>
+  );
+};
+
+export default App;
